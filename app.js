2cf534373d0c38b48ec17292b7e55b8d859bfb6f
Morgan Ney <morganney@gmail.com>
2014-02-15 17:30:51 -0800

    putting in wild first time

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express'),
     restbus = require('restbus'),
     app = express();
 
-
+// Configuration
 app.enable('trust proxy');
 app.set('view engine', 'jade');
 app.locals({pretty: true});
 
+// Middleware & Error Handling
 app.use(express.logger('dev'));
 app.use(express.compress());
-//app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 app.use(function(req, res, next) {
   res.send(404);
@@ -19,11 +19,13 @@ app.use(function(err, req, res, next) {
   res.send(500);
 });
 
+
+// Routing
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-
+// Get to work
 app.listen('3000', function() {
   console.log('restbus.info listening on port 3000');
   restbus.listen('3535', function() {
